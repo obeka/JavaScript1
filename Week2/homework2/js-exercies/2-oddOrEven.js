0463061e4ee62501fba5637c39878b6c729de045
Ömer <omerbkk06@gmail.com>
2020-04-04 11:27:46 +0200

    Updated odd or even homework

--- Week2/homework2/js-exercies/2-oddOrEven.js
+++ Week2/homework2/js-exercies/2-oddOrEven.js
@@ -1,9 +1,8 @@
 'use strict'
-let nums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-for(let i = 0; i < nums.length; i++) {
-    if(nums[i] % 2 === 0) {
-        console.log(`The number ${nums[i]} is even.`)
+for(let i = 0; i < 21; i++) {
+    if(i % 2 === 0) {
+        console.log(`The number ${i} is even.`)
     } else {
-        console.log(`The number ${nums[i]} is odd.`)
+        console.log(`The number ${i} is odd.`)
     }
 }
